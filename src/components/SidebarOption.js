8f541d03312d922a0d5043660a1e543f206a43f0
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-02-24 14:17:59 +0100

    Firebase hooks, Redux appSlice for roomId added

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -3,11 +3,38 @@ import {
   SidebarOptionChannel,
   SidebarOptionContainer,
 } from "../styles/SidebarOption.style";
+import { db } from "../firebase";
+import { useDispatch } from "react-redux";
+import { enterRoom } from "../features/appSlice";
 
-function SidebarOption({ Icon, title, addChannelOption }) {
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
+
   return (
     <div>
-      <SidebarOptionContainer>
+      <SidebarOptionContainer
+        onClick={addChannelOption ? addChannel : selectChannel}
+      >
         {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
         {Icon ? (
           <h3>{title}</h3>
